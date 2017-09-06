cf28f14bf2b54a8f37a9c1353feee9b9734a990e
pkr9707 <31675670+pkr9707@users.noreply.github.com>
2017-09-06 10:40:06 +0900

    Add files via upload

---
+++ P_1st_Assignment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+	printf("student id number: 2017301027\n");
+	printf("name: Park Kyounglim\n");
+	printf("mobile phone number: 010 3090 5251\n");
+	printf("GitHub address: https://github.com/pkr9707");
+	getchar();
+	return0;
+
+}
